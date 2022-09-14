a57ac9e919edfcbcf97390b6568010419a0ef309
Paul1k-Asante <107471494+Paul1k-Asante@users.noreply.github.com>
2022-09-13 19:12:30 -0700

    Add files via upload

---
+++ Starting/Testing Python 1.py
@@ -0,0 +1,67 @@
+﻿# All on Print strings and concatenating it. 
+
+#hey.....there is nothing here. WAKE UP!!!  / commenting out or 
+
+#print("Hello world!!!!!!")
+#print("I am Iron Man")
+#print("No, I am Tony Stark")
+#print("No, I am Poppy")
+
+#We are practicing python.....IT's AWESOME!!!
+
+#print("Hello Gloria") # Print("It in, what comes!!!!!")
+#But if;
+
+#print("It In, What comes!!!!") print("Hello Gloria") Nothing comes.
+
+#print("It in, What comes!!! " + "Hello Gloria")
+#in the same way print("It in, What comes!!! " + 'The Variable')
+# variable = "name"
+# print(variable)
+# i.e. print("It in, What comes!!! " + variable)
+# 
+
+#print("Hello world!!!!!!")
+#print(No, I am Poppy)  = This is SyntaxError
+
+print("""I am Iron Man. No,
+I am Tony Stark.
+No, I am poppy.""")
+
+print("I am Iron Man" + "No, I am Tony Stark" + "No, I am Poppy")
+
+print("I am Iron Man. " + "No, I an Tony Stark. " + "No, I am Poppy.")
+
+print("I am Iron Man. \n" + "No, I an Tony Stark.\n " + "No, I am Poppy.")
+
+print("I am Iron Man. \n" + "No, I an Tony Stark.\n" + "No, I am Poppy.")
+
+print("I am Poppy" * 100)
+
+print("I am Poppy, " * 100)
+
+print("I am Poppy \n" * 10)
+
+print("I am Iron Man. No, I am Tony Stark. no, I am Poppy.")
+
+#print("I am Iron Man.
+ #No, I am Tony Stark.
+  #no, I am Poppy.")
+
+print("a" + "n")
+print("a " + "b")
+print("a and b \n c and d")
+print("a and b\nc and d")
+print("a and b \n" + "c and d") # without space so the next line don't get space.
+print("a and b \n " + "c and d") # with space, so the next line get space.
+
+# \n called new line indicator or next line
+# "a" and
+# "a " this is a with space 
+# space is a character NB;
+# "a and b \n c and d" means from \n moves it to the next line with space
+# "a and b \nc and d" means from \n moves it to the next line with space
+# because there is no space after the \n but \nc and d
+
+print("I am Poppy \n" * (5))
+
