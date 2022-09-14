194e24ad09e79d6bed057fea484ceb35ef3984a2
Paul1k-Asante <107471494+Paul1k-Asante@users.noreply.github.com>
2022-09-13 19:15:38 -0700

    Add files via upload

---
+++ Starting/Testing Python 4.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+keyboard.press('j')
+keyboard.release('j')
+
+keyboard.press('l')
+keyboard.release('l')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
